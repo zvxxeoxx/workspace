6cfe267e75966b613a8f60aba888849c025959d5
zvxxeoxx <adc1355@gmail.com>
2025-01-22 09:17:08 +0900

    Update homework

--- ch99_homework/03_conversion.c
+++ ch99_homework/03_conversion.c
@@ -14,13 +14,19 @@
  */
 
 #include <stdio.h>
-int FtoC(double fVal);
 
-int FtoC(double fVal) {
-    
+ 
+
+double FtoC(double fVal) {
+    double cVal = (5.0*(fVal - 32.0)) / 9.0;
+    return cVal;
 }
 
 int main() {
-    
+  double fVal, cVal;
+  printf("화씨 온도를 입력하세요: ");
+  scanf("%lf", &fVal);
+  cVal = FtoC(fVal);
+  printf("화씨온도: %.1lf, 섭씨온도: %.1lf\n", fVal, cVal);
 
 }
